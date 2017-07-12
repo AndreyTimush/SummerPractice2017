2153a89fc1f718b86356a298aa5e17048af60f38
andrik2797 <andrikshumit@gmail.com>
2017-07-13 01:06:31 +0300

    start

---
+++ Chat/src/lesson170705/Hello.java
@@ -0,0 +1,14 @@
+package lesson170705;
+
+/**
+ * Created by andrew on 13.07.17.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    }
+
+}
